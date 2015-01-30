bd05cc80b1669e456c5bd938b4787712c1d306a1
kylelowtharp <klv9700@saintjoe.edu>
2015-01-30 15:44:29 -0500

    add clear method

--- Point.java
+++ Point.java
@@ -5,4 +5,8 @@ public class Point {
 	  System.out.println("X coordinate: " +  this.x);
 	  System.out.println("Y coordinate: " +  this.y);
 	}
+	public void clear() {
+	 x = 0.0;
+	 y = 0.0;
+	}
 }
